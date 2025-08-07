12168a883f1972f0220d0468736b0a1602a44968
veronikavanivska <veronikavanivska2005@gmail.com>
2025-08-07 14:49:22 +0200

    Add Builder and first draft of RetryPolicy

--- src/main/java/com/vanivska/retryhelper/BackOffStrategy.java
+++ src/main/java/com/vanivska/retryhelper/BackOffStrategy.java
@@ -1,4 +1,10 @@
 package com.vanivska.retryhelper;
 
+import java.time.Duration;
+
 public interface BackOffStrategy {
+
+    Duration nextAttemptTime(int attempt);
+
+
 }
--- src/main/java/com/vanivska/retryhelper/RetryPolicy.java
+++ src/main/java/com/vanivska/retryhelper/RetryPolicy.java
@@ -7,10 +7,41 @@ import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class RetryPolicy {
-    int maxAttempts;
-    Duration time;
-    Predicate<Throwable> predicate;
+    private final int maxAttempts;
+    private final Predicate<Throwable> predicate;
+    private final BackOffStrategy backOffStrategy;
+
+    private RetryPolicy(Builder builder){
+        this.maxAttempts = builder.maxAttempts;
+        this.predicate = builder.predicate;
+        this.backOffStrategy = builder.backOffStrategy;
+    }
+    public int getMaxAttempts() { return maxAttempts; }
+    public BackOffStrategy getBackOffStrategy() { return backOffStrategy; }
+    public Predicate<Throwable> getPredicate() { return predicate; }
 
 
 
+    public static class Builder {
+        private int maxAttempts =3 ;
+        private Predicate<Throwable> predicate = e -> true;
+        private BackOffStrategy backOffStrategy;
+
+        public Builder maxAttempts(int maxAttempts) {
+            this.maxAttempts = maxAttempts;
+            return this;
+        }
+        public Builder predicate(Predicate<Throwable> predicate) {
+            this.predicate = predicate;
+            return this;
+        }
+        public Builder backOffStrategy(BackOffStrategy backOffStrategy) {
+            this.backOffStrategy = backOffStrategy;
+            return this;
+        }
+        public RetryPolicy build() {
+            if (maxAttempts <= 0) throw new IllegalArgumentException("maxAttempts must be > 0");
+            return new RetryPolicy(this);
+        }
+    }
 }
